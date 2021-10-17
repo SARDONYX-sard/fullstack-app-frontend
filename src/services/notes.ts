53ef695e411fbd30c8512f2a511e13e903f59911
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-10-18 02:54:52 +0900

    refactor: modify baseUrl

--- src/services/notes.ts
+++ src/services/notes.ts
@@ -1,22 +1,11 @@
 import axios from "axios";
 import type { NoteService } from "../types/note";
 
-const baseUrl = "http://localhost:3001/notes";
-
-// const getAll2 = () => {
-//   const request = axios.get(baseUrl);
-//   return request.then((response) => response.data);
-// };
+const baseUrl = "/api/notes";
 
 const getAll = () => {
   const request = axios.get<NoteService[]>(baseUrl);
-  const nonExisting = {
-    id: 10000,
-    content: "This note is not saved to server",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  };
-  return request.then((response) => response.data.concat(nonExisting));
+  return request.then((response) => response.data);
 };
 
 const create = (newObject: Omit<NoteService, "id">) => {
