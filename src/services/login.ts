dd8439bb1eae03cc2facd3a98f744fec48212bb6
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-10-23 06:47:29 +0900

    feat: add login post api

---
+++ src/services/login.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import type { User } from '../types/note'
+
+const baseUrl = '/api/login'
+
+const login = async (credentials: { username: string; password: string }) => {
+  const response = await axios.post(baseUrl, credentials)
+  return response.data as User
+}
+
+export { login }
