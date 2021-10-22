03360b797a7cbfeb82660b8aeea13a9078852bde
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-10-23 06:42:38 +0900

    feat: add `Togglable` component

---
+++ src/components/Togglable.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+type Props = {
+  buttonLabel: string
+  children: React.ReactNode
+}
+
+const Togglable = ({ buttonLabel, children }: Props) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default Togglable
