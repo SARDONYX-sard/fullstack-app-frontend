4c560801836fb372608a9e9cacd1ddc8c968c92b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-10-23 06:42:17 +0900

    feat: token function

--- src/services/notes.ts
+++ src/services/notes.ts
@@ -1,25 +1,30 @@
 import axios from 'axios'
-import type { NoteService } from '../types/note'
-
+import type { NoteService, PostNote } from '../types/note'
 const baseUrl = '/api/notes'
+
+let token: string
+
+const setToken = (newToken: string) => {
+  token = `bearer ${newToken}`
+}
 
 const getAll = () => {
   const request = axios.get<NoteService[]>(baseUrl)
   return request.then(response => response.data)
 }
 
-const create = (newObject: Omit<NoteService, 'id'>) => {
-  const request = axios.post<NoteService>(baseUrl, newObject)
+const create = (newObject: PostNote) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const request = axios.post<NoteService>(baseUrl, newObject, config)
   return request.then(response => response.data)
 }
 
-const update = (id: number, newObject: Omit<NoteService, 'id'>) => {
+const update = (id: number, newObject: PostNote) => {
   const request = axios.put<NoteService>(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
 
-export default {
-  getAll,
-  create,
-  update,
-}
+export { getAll, create, update, setToken }
