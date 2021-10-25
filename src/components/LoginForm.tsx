29010f810f5859aa6d667c39e8987eb857b3dcaf
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-10-25 15:06:23 +0900

    feat(login): add id

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -12,17 +12,18 @@ const LoginForm = ({ handleSubmit, handleUsernameChange, handlePasswordChange, u
   return (
     <div>
       <h2>Login</h2>
-
       <form onSubmit={handleSubmit}>
         <div>
           username
-          <input value={username} onChange={handleUsernameChange} />
+          <input id="username" value={username} onChange={handleUsernameChange} />
         </div>
         <div>
           password
-          <input type="password" value={password} onChange={handlePasswordChange} />
+          <input id="password" type="password" value={password} onChange={handlePasswordChange} />
         </div>
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">
+          login
+        </button>
       </form>
     </div>
   )
