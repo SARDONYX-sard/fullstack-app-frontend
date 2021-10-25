9102f4b5840fbbca2b61da10afe73c8ccded4185
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-10-25 19:10:35 +0900

    chore: add cypress files

---
+++ cypress/support/commands.ts
@@ -0,0 +1,49 @@
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      login(credentials: Credentials): void
+      createNote(newNote: NewNote): void
+    }
+  }
+}
+
+type Credentials = {
+  username: string
+  password: string
+}
+
+Cypress.Commands.add('login', ({ username, password }: Credentials) => {
+  cy.request('POST', 'http://localhost:3001/api/login', {
+    username,
+    password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedNoteAppUser', JSON.stringify(body))
+    cy.visit('http://localhost:3001')
+  })
+})
+
+type NewNote = {
+  content: string
+  important: boolean
+}
+
+Cypress.Commands.add('createNote', ({ content, important }: NewNote) => {
+  const userData = localStorage.getItem('loggedNoteAppUser')
+  if (userData === null) {
+    return
+  }
+
+  cy.request({
+    url: 'http://localhost:3001/api/notes',
+    method: 'POST',
+    body: { content, important },
+    headers: {
+      Authorization: `bearer ${JSON.parse(userData).token}`
+    }
+  })
+
+  cy.visit('http://localhost:3001')
+})
+
+export {}
