bc318d2cef4a4e4c0c19ddf6c223f991c1b80060
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-11-01 18:43:12 +0900

    BREAKING CHANGE: fetch from DB

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
+import { initializeNotes } from './reducers/noteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
+
   return (
     <div>
       <NewNote />
